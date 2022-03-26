4a47b9fc8ee2d6be52bf834be2414f7d72fa9a84
Belovus <88674801+Belovus@users.noreply.github.com>
2022-03-26 12:07:28 +0300

    add_main&change_readme

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import Authorization from "./pages/authorization"
 import Registration from "./pages/registration"
 import Chats from "./pages/chats"
 import Profile from "./pages/profile"
+import Main from "./pages/main"
 
 import './global.sass'
 
 const routes = {
-  "/400": Mistake,
+  "/404": Mistake,
   "/500": Mistake,
   "/login": Authorization,
   "/registration": Registration,
   "/chats": Chats,
-  "/profile": Profile
+  "/profile": Profile,
+  "/": Main
 }
 
 const app = document.getElementById("app")
